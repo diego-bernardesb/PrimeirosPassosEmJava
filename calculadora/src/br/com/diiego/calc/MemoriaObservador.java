07b17ba497709a02bac97d795691697ecb2d875d
diego-bernardesb <diegobbabo@gmail.com>
2021-10-25 12:00:51 -0300

    Clone da Cacluladora do MAC utilizando Swing

---
+++ calculadora/src/br/com/diiego/calc/MemoriaObservador.java
@@ -0,0 +1,6 @@
+package br.com.diiego.calc;
+@FunctionalInterface
+public interface MemoriaObservador {
+
+	public void valorAlterado (String valor);
+}
---
+++ calculadora/src/br/com/diiego/calc/modelo/Memoria.java
@@ -0,0 +1,119 @@
+package br.com.diiego.calc.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.diiego.calc.MemoriaObservador;
+
+public class Memoria {
+
+	private enum TipoComando {
+		ZERAR, SINAL, NUMERO, SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO, IGUAL,VIRGULA
+	}
+	
+	private static final Memoria instancia = new Memoria();
+
+	private final List<MemoriaObservador> observadores = new ArrayList<>();
+	private TipoComando ultimaOperacao = null;
+	private boolean substituir = false;
+	private String textoAtual = "";
+	private String textoBuffer = "";
+
+	private Memoria() {
+
+	}
+
+	public static Memoria getInstancia() {
+		return instancia;
+	}
+
+	public void adicionarObservador(MemoriaObservador observador) {
+		observadores.add(observador);
+	}
+
+	public String getTextoAtual() {
+		return textoAtual.isEmpty() ? "0" : textoAtual;
+	}
+
+	public void processarComando(String texto) {
+		
+		TipoComando tipoComando = detectarComando(texto);
+		
+		if(tipoComando == null) {
+			return;
+		}else if(tipoComando == TipoComando.ZERAR) {
+			textoAtual = "";
+			textoBuffer = "";
+			substituir = false;
+			ultimaOperacao = null;
+		}else if(tipoComando == TipoComando.SINAL && textoAtual.contains("-")) {
+			textoAtual = textoAtual.substring(1);			
+		}else if(tipoComando == TipoComando.SINAL && !textoAtual.contains("-")) {	
+			textoAtual = "-" + textoAtual;
+		}else if(tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+			textoAtual = substituir ? texto : textoAtual + texto;
+			substituir = false;
+		} else {
+			substituir = true;
+			textoAtual = obterResultado();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
+		}
+
+		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private String obterResultado() {
+		if(ultimaOperacao == null || ultimaOperacao == TipoComando.IGUAL) {
+			return textoAtual;
+		}
+		double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+		double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+		
+		double resultado = 0;
+		
+		if(ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		}else if(ultimaOperacao == TipoComando.SUBTRACAO) {
+			resultado = numeroBuffer - numeroAtual;
+		}else if(ultimaOperacao == TipoComando.DIVISAO) {
+			resultado = numeroBuffer / numeroAtual;
+		}else if(ultimaOperacao == TipoComando.MULTIPLICACAO) {
+			resultado = numeroBuffer * numeroAtual;
+		}
+		
+		String resultadoString = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = resultadoString.endsWith(",0");		
+		return inteiro ? resultadoString.replace(",0", "") : resultadoString;
+	}
+
+	private TipoComando detectarComando(String texto) {
+		if(textoAtual.isEmpty() && texto == "0") {
+			return null;
+		}
+		
+		try {
+			Integer.parseInt(texto);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			if ("AC".equals(texto)) {
+				return TipoComando.ZERAR;
+			}else if("/".equals(texto)) {
+				return TipoComando.DIVISAO;
+			}else if("*".equals(texto)) {
+				return TipoComando.MULTIPLICACAO;
+			}else if("-".equals(texto)) {
+				return TipoComando.SUBTRACAO;
+			}else if("+".equals(texto)) {
+				return TipoComando.SOMA;
+			}else if("=".equals(texto)) {
+				return TipoComando.IGUAL;
+			}else if("±".equals(texto)) {
+				return TipoComando.SINAL;
+			}else if(",".equals(texto) && !textoAtual.contains(",")) {
+				return TipoComando.VIRGULA;
+			}
+		}
+		return null;
+	}
+}
